8b61d91140fe0232edda8dc6505ab92c1c97959a
John Wilson <jow2@stir.ac.uk>
2018-11-28 13:38:04 +0000

    Add functions presentation .Rmd and environments example  script

---
+++ environments.R
@@ -0,0 +1,30 @@
+### Environments
+
+#### Global env
+
+
+#----------#
+
+x_plus_x <- function(x, y) {
+  # x should be a dfgtrh
+  # y is the resulys
+  x + x
+
+
+  }
+x_plus_x(x = number)
+
+#----------#
+
+x_plus_n1 <- function(x, y) x + y
+
+x_plus_n1(x = number, y = number2)
+
+#----------#
+
+x_plus_n2 <- function(x) {
+  number <- 6
+}
+
+x_plus_n2(number)
+
